6dbcb222c527617238cac304ef57d1326877df45
Kundan Roy <kundan.roy@diaspark.com>
2017-03-05 14:53:10 +0530

    new seed file

--- database/migrations/2017_03_02_090356_create_assignments_table.php
+++ database/migrations/2017_03_02_090356_create_assignments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePapersTable extends Migration
+class CreateAssignmentsTable extends Migration
 {
     /**
      * Run the migrations.
