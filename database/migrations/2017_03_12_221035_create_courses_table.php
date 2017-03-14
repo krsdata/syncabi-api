64a5530b31ad6498c93abc71c87e3ee84d3cde66
krsdata <krsdata.net@gmail.com>
2017-03-15 02:05:27 +0530

    migration updated

--- database/migrations/2017_03_12_221035_create_courses_table.php
+++ database/migrations/2017_03_12_221035_create_courses_table.php
@@ -15,8 +15,8 @@ class CreateCoursesTable extends Migration {
 		Schema::create('courses', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('course_name')->unsigned()->nullable();
-			$table->string('course_code')->unsigned()->nullable();
+			$table->string('course_name')->nullable();
+			$table->string('course_code')->nullable();
 			$table->integer('professor_id')->nullable()->unsigned()->index('courses_professor_id_foreign');
 			$table->boolean('status')->default(1);
 			$table->timestamps();
