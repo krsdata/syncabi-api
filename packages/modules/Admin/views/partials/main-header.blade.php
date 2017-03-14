cf8652fc0708fe2abf9de6c16c5c7402350ff6c9
krsdata <krsdata.net@gmail.com>
2017-03-15 02:56:24 +0530

    migration updated

--- packages/modules/Admin/views/partials/main-header.blade.php
+++ packages/modules/Admin/views/partials/main-header.blade.php
@@ -2,9 +2,9 @@
         <!-- sign  -->
         <a class="logo" href="{{ url('admin') }}">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>Udex</b></span>
+          <span class="logo-mini"><b>API</b></span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Udex Admin</b></span>
+          <span class="logo-lg"><b>Admin Panel</b></span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav role="navigation" class="navbar navbar-static-top">
